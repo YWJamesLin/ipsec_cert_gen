dc7820c34a26da82bb7b5b829b623e86f762a1c6
YWJamesLin <YWJamesLin@gmail.com>
2016-01-09 12:57:38 +0800

    With more variable parameter

--- generate.sh
+++ generate.sh
@@ -1,58 +1,64 @@
 #! /bin/bash
 # Generate IPSec Certification
 # 2015.09.24 Created By YWJamesLin
+# 2016.01.09 Last Modified
 
 IPSec_Program="ipsec"
+StrongSwan_DIR="/etc"
+
 KeyType="rsa"
 KeySize="4096"
-StrongSwan_DIR="/etc"
-Server_San_Flag_String="--flag serverAuth --flag ikeIntermediate"
-Client_San_Flag_String=""
-ClientName="Client"
+
+CAName="StrongSwanCA"
+LeftName="Server"
+RightName="Client"
+
+CALifeTime="3650"
+LeftLifeTime="3650"
+RightLifeTime="3650"
+
+Left_San_Flag_String="--flag serverAuth --flag ikeIntermediate"
+Right_San_Flag_String=""
 
 GenCA="1"
-GenServer="1"
-GenClient="1"
+GenLeft="1"
+GenRight="1"
 GenTar="1"
 GenP12="1"
 
 cd ${StrongSwan_DIR}
 
 if [ ${GenCA} == "1" ]; then
-  ${IPSec_Program} pki --gen --type ${KeyType} --size ${KeySize} --outform der > ipsec.d/private/strongswanKey.der
-  chmod 600 ipsec.d/private/strongswanKey.der
+  ${IPSec_Program} pki --gen --type ${KeyType} --size ${KeySize} --outform pem > ipsec.d/private/${CAName}Key.pem
+  chmod 600 ipsec.d/private/${CAName}Key.pem
 
-  ${IPSec_Program} pki --self --ca --lifetime 3650 --in ipsec.d/private/strongswanKey.der --type ${KeyType} --dn "C=NL, O=Example Company, CN=strongswan Root CA" --outform der > ipsec.d/cacerts/strongswanCert.der
+  ${IPSec_Program} pki --self --ca --lifetime ${CALifeTime} --in ipsec.d/private/${CAName}Key.pem --type ${KeyType} --dn "C=NL, O=Example Organization, CN=strongswan Root CA" --outform pem > ipsec.d/cacerts/${CAName}Cert.pem
 
-  ${IPSec_Program} pki --print --in ipsec.d/cacerts/strongswanCert.der
+  ${IPSec_Program} pki --print --in ipsec.d/cacerts/${CAName}Cert.pem
 fi
 
-if [ ${GenServer} == "1" ]; then
-  ${IPSec_Program} pki --gen --type ${KeyType} --size ${KeySize} --outform der > ipsec.d/private/vpnHostKey.der
-  chmod 600 ipsec.d/private/vpnHostKey.der
+if [ ${GenLeft} == "1" ]; then
+  ${IPSec_Program} pki --gen --type ${KeyType} --size ${KeySize} --outform pem > ipsec.d/private/${LeftName}Key.pem
+  chmod 600 ipsec.d/private/${LeftName}Key.pem
 
-  ${IPSec_Program} pki --pub --in ipsec.d/private/vpnHostKey.der --type ${KeyType} | ${IPSec_Program} pki --issue --lifetime 730 --cacert ipsec.d/cacerts/strongswanCert.der --cakey ipsec.d/private/strongswanKey.der --dn "C=NL, O=Example Company, CN=vpn.example.org" ${Server_San_Flag_String} --outform der > ipsec.d/certs/vpnHostCert.der
+  ${IPSec_Program} pki --pub --in ipsec.d/private/${LeftName}Key.pem --type ${KeyType} | ${IPSec_Program} pki --issue --lifetime ${LeftLifeTime} --cacert ipsec.d/cacerts/${CAName}Cert.pem --cakey ipsec.d/private/${CAName}Key.pem --dn "C=NL, O=Example Organization, CN=vpn.example.org" ${Left_San_Flag_String} --outform pem > ipsec.d/certs/${LeftName}Cert.pem
 
-  ${IPSec_Program} pki --print --in ipsec.d/certs/vpnHostCert.der
+  ${IPSec_Program} pki --print --in ipsec.d/certs/${LeftName}Cert.pem
 fi
 
-if [ ${GenClient} == "1" ]; then
-  ${IPSec_Program} pki --gen --type ${KeyType} --size ${KeySize} --outform der > ipsec.d/private/${ClientName}Key.der
-  chmod 600 ipsec.d/private/${ClientName}Key.der
+if [ ${GenRight} == "1" ]; then
+  ${IPSec_Program} pki --gen --type ${KeyType} --size ${KeySize} --outform pem > ipsec.d/private/${RightName}Key.pem
+  chmod 600 ipsec.d/private/${RightName}Key.pem
 
-  ${IPSec_Program} pki --pub --in ipsec.d/private/${ClientName}Key.der --type ${KeyType} | ${IPSec_Program} pki --issue --lifetime 730 --cacert ipsec.d/cacerts/strongswanCert.der --cakey ipsec.d/private/strongswanKey.der --dn "C=NL, O=Example Company, CN=example@example.org" ${Client_San_Flag_String} --outform der > ipsec.d/certs/${ClientName}Cert.der
+  ${IPSec_Program} pki --pub --in ipsec.d/private/${RightName}Key.pem --type ${KeyType} | ${IPSec_Program} pki --issue --lifetime ${RightLifeTime} --cacert ipsec.d/cacerts/${CAName}Cert.pem --cakey ipsec.d/private/${CAName}Key.pem --dn "C=NL, O=Example Organization, CN=example@example.org" ${Right_San_Flag_String} --outform pem > ipsec.d/certs/${RightName}Cert.pem
   
-  ${IPSec_Program} pki --print --in ipsec.d/certs/${ClientName}Cert.der
+  ${IPSec_Program} pki --print --in ipsec.d/certs/${RightName}Cert.pem
 fi
 
 if [ ${GenP12} == "1" ]; then
-  openssl ${KeyType} -inform DER -in ipsec.d/private/${ClientName}Key.der -out ipsec.d/private/${ClientName}Key.pem -outform PEM
-  openssl x509 -inform DER -in ipsec.d/certs/${ClientName}Cert.der -out ipsec.d/certs/${ClientName}Cert.pem -outform PEM
-  openssl x509 -inform DER -in ipsec.d/cacerts/strongswanCert.der -out ipsec.d/cacerts/strongswanCert.pem -outform PEM
-
-  openssl pkcs12 -export  -inkey ipsec.d/private/${ClientName}Key.pem -in ipsec.d/certs/${ClientName}Cert.pem -name "${ClientName}'s VPN Certificate"  -certfile ipsec.d/cacerts/strongswanCert.pem -caname "strongswan Root CA" -out ~/${ClientName}.p12
+  openssl pkcs12 -export  -inkey ipsec.d/private/${RightName}Key.pem -in ipsec.d/certs/${RightName}Cert.pem -name "${RightName}'s VPN Certificate"  -certfile ipsec.d/cacerts/${CAName}Cert.pem -caname "strongswan Root CA" -out ~/${RightName}.p12
 fi
 
 if [ ${GenTar} == "1" ]; then
-  tar zcvf ~/${ClientName}_Linux.tar.gz ipsec.d/private/${ClientName}Key.pem ipsec.d/certs/${ClientName}Cert.pem ipsec.d/cacerts/strongswanCert.pem
+  tar zcvf ~/${RightName}_Linux.tar.gz ipsec.d/private/${RightName}Key.pem ipsec.d/certs/${RightName}Cert.pem ipsec.d/cacerts/${CAName}Cert.pem
 fi
