22cc9d98698f8876950675a97622414f5b174492
YWJamesLin <YWJamesLin@gmail.com>
2016-06-09 20:00:07 +0800

    Refactor generation parameters Move RSA to ECDSA

--- generate.sh
+++ generate.sh
@@ -3,22 +3,29 @@
 # 2015.09.24 Created By YWJamesLin
 # 2016.01.09 Last Modified
 
-IPSec_Program="ipsec"
-StrongSwan_DIR="/etc"
+IpsecProgram="ipsec"
+StrongSwanDir="/etc"
 
-KeyType="rsa"
-KeySize="4096"
+KeyType="ecdsa"
+KeySize="521"
 
 CAName="StrongSwanCA"
 LeftName="Server"
 RightName="Client_${LeftName}"
 
+Country="TW"
+Organization="Organization"
+CASubjectName="C=${Country}, O=${Organization}, CN=${CAName}"
+LeftSubjectName="C=${Country}, O=${Organization}, CN=${LeftName}"
+RightSubjectName="C=${Country}, O=${Organization}, CN=${RightName}"
+
 CALifeTime="3650"
 LeftLifeTime="3650"
 RightLifeTime="3650"
 
-Left_San_Flag_String="--flag serverAuth --flag ikeIntermediate"
-Right_San_Flag_String=""
+CASanFlagString=""
+LeftSanFlagString="--flag serverAuth --flag ikeIntermediate"
+RightSanFlagString=""
 
 GenCA="1"
 GenLeft="1"
@@ -26,37 +33,37 @@ GenRight="1"
 GenTar="1"
 GenP12="1"
 
-cd ${StrongSwan_DIR}
+cd ${StrongSwanDir}
 
 if [ ${GenCA} == "1" ]; then
-  ${IPSec_Program} pki --gen --type ${KeyType} --size ${KeySize} --outform pem > ipsec.d/private/${CAName}Key.pem
+  ${IpsecProgram} pki --gen --type ${KeyType} --size ${KeySize} --outform pem > ipsec.d/private/${CAName}Key.pem
   chmod 600 ipsec.d/private/${CAName}Key.pem
 
-  ${IPSec_Program} pki --self --ca --lifetime ${CALifeTime} --in ipsec.d/private/${CAName}Key.pem --type ${KeyType} --dn "C=NL, O=Example Organization, CN=strongswan Root CA" --outform pem > ipsec.d/cacerts/${CAName}Cert.pem
+  ${IpsecProgram} pki --self --ca --lifetime ${CALifeTime} --in ipsec.d/private/${CAName}Key.pem --type ${KeyType} --dn "${CASubjectName}" ${CASanFlagString} --outform pem > ipsec.d/cacerts/${CAName}Cert.pem
 
-  ${IPSec_Program} pki --print --in ipsec.d/cacerts/${CAName}Cert.pem
+  ${IpsecProgram} pki --print --in ipsec.d/cacerts/${CAName}Cert.pem
 fi
 
 if [ ${GenLeft} == "1" ]; then
-  ${IPSec_Program} pki --gen --type ${KeyType} --size ${KeySize} --outform pem > ipsec.d/private/${LeftName}Key.pem
+  ${IpsecProgram} pki --gen --type ${KeyType} --size ${KeySize} --outform pem > ipsec.d/private/${LeftName}Key.pem
   chmod 600 ipsec.d/private/${LeftName}Key.pem
 
-  ${IPSec_Program} pki --pub --in ipsec.d/private/${LeftName}Key.pem --type ${KeyType} | ${IPSec_Program} pki --issue --lifetime ${LeftLifeTime} --cacert ipsec.d/cacerts/${CAName}Cert.pem --cakey ipsec.d/private/${CAName}Key.pem --dn "C=NL, O=Example Organization, CN=vpn.example.org" ${Left_San_Flag_String} --outform pem > ipsec.d/certs/${LeftName}Cert.pem
+  ${IpsecProgram} pki --pub --in ipsec.d/private/${LeftName}Key.pem --type ${KeyType} | ${IpsecProgram} pki --issue --lifetime ${LeftLifeTime} --cacert ipsec.d/cacerts/${CAName}Cert.pem --cakey ipsec.d/private/${CAName}Key.pem --dn "${LeftSubjectName}" ${LeftSanFlagString} --outform pem > ipsec.d/certs/${LeftName}Cert.pem
 
-  ${IPSec_Program} pki --print --in ipsec.d/certs/${LeftName}Cert.pem
+  ${IpsecProgram} pki --print --in ipsec.d/certs/${LeftName}Cert.pem
 fi
 
 if [ ${GenRight} == "1" ]; then
-  ${IPSec_Program} pki --gen --type ${KeyType} --size ${KeySize} --outform pem > ipsec.d/private/${RightName}Key.pem
+  ${IpsecProgram} pki --gen --type ${KeyType} --size ${KeySize} --outform pem > ipsec.d/private/${RightName}Key.pem
   chmod 600 ipsec.d/private/${RightName}Key.pem
 
-  ${IPSec_Program} pki --pub --in ipsec.d/private/${RightName}Key.pem --type ${KeyType} | ${IPSec_Program} pki --issue --lifetime ${RightLifeTime} --cacert ipsec.d/cacerts/${CAName}Cert.pem --cakey ipsec.d/private/${CAName}Key.pem --dn "C=NL, O=Example Organization, CN=example@example.org" ${Right_San_Flag_String} --outform pem > ipsec.d/certs/${RightName}Cert.pem
+  ${IpsecProgram} pki --pub --in ipsec.d/private/${RightName}Key.pem --type ${KeyType} | ${IpsecProgram} pki --issue --lifetime ${RightLifeTime} --cacert ipsec.d/cacerts/${CAName}Cert.pem --cakey ipsec.d/private/${CAName}Key.pem --dn "${RightSubjectName}" ${RightSanFlagString} --outform pem > ipsec.d/certs/${RightName}Cert.pem
   
-  ${IPSec_Program} pki --print --in ipsec.d/certs/${RightName}Cert.pem
+  ${IpsecProgram} pki --print --in ipsec.d/certs/${RightName}Cert.pem
 fi
 
 if [ ${GenP12} == "1" ]; then
-  openssl pkcs12 -export  -inkey ipsec.d/private/${RightName}Key.pem -in ipsec.d/certs/${RightName}Cert.pem -name "${RightName}'s VPN Certificate"  -certfile ipsec.d/cacerts/${CAName}Cert.pem -caname "strongswan Root CA" -out ~/${RightName}.p12
+  openssl pkcs12 -export  -inkey ipsec.d/private/${RightName}Key.pem -in ipsec.d/certs/${RightName}Cert.pem -name "${RightName}'s VPN Certificate"  -certfile ipsec.d/cacerts/${CAName}Cert.pem -caname ${CAName} -out ~/${RightName}.p12
 fi
 
 if [ ${GenTar} == "1" ]; then
